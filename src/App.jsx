9dfc9379be4c65adb7fe9d795f450fb7a59bf9fd
beehsouza-th <brenda.souza@estudante.ifms.edu.br>
2024-06-25 17:24:48 -0400

    tres estados

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
+import { useState } from "react";
 import "../src/globals.css";
 
 
 export default function App() {
 
-   
+   const[nome, setNome] = useState("milho");
+   const[numero, setNumero] = useState(77);
+   const[CPF, setCPF] = useState(0);
 
+    const mudarNome = () =>{
+    setNome("pipoca")
+    }
 
-    return (
+    const mudarNumero =() =>{
+        setNumero(1)
+    }
+    
+    const mudarCPF = () =>{
+        setCPF(88)
+    }
+    
+    return(
        <div>
+      {nome}
+      <button onClick = {mudarNome} > Mudar </button>
 
+      {numero}
+      <button onClick = {mudarNumero} > Mudar </button>
+
+      {CPF}
+      <button onClick = {mudarCPF} > Mudar </button>
        </div>
     );
 }
